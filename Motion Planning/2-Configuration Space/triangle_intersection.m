b7937fdd2ba3551cfba83bd59d117c911380c420
YangTao848 <31799659+YangTao848@users.noreply.github.com>
2017-11-27 20:29:58 +0800

    Update triangle_intersection.m

--- Motion Planning/2-Configuration Space/triangle_intersection.m
+++ Motion Planning/2-Configuration Space/triangle_intersection.m
@@ -1,62 +1,140 @@
+%http://blog.csdn.net/yessharing/article/details/55225402
 function flag = triangle_intersection(P1, P2)
 % triangle_test : returns true if the triangles overlap and false otherwise
+for i = 1:3
 
-%%% All of your code should be between the two lines of stars.
-% *******************************************************************
+    for j = 1:3
 
-flag = true;
-for i=1:3
-   X=P1;
-   X(i,:)=[];
-   
-   a=X(2,:)-X(1,:);
-   if a(1)==0
-       p1=P1(i,1)- X(1,1);
-       p2=P2(:,1)- X(1,1);
-   else
-       a=a(2)/a(1);
-       b=X(1,2)-a*X(1,1);
+        A1 = P1(mod(j+1, 3)+1, 2) - P1(mod(j, 3)+1, 2);
 
-       p1=P1(i,2)- a*P1(i,1)-b;
-       p2=P2(:,2)- a*P2(:,1)-b;   
-     
-   end
-   
-   p1=p1/abs(p1);
-   p2=sum(p2./abs(p2));
-   p2*p1;
-   if p2*p1==-3
-       flag=false;
-       return
-   end
-end 
+        B1 = P1(mod(j, 3)+1, 1) - P1(mod(j+1, 3)+1, 1);
+
+        C1 = A1*P1(mod(j, 3)+1, 1)+B1*P1(mod(j, 3)+1, 2);
 
 
-for i=1:3
-   X=P2;
-   X(i,:)=[];
-   
-   a=X(2,:)-X(1,:);
-   if a(1)==0
-       p2=P2(i,1)- X(1,1);
-       p1=P1(:,1)- X(1,1);
-   else
-       a=a(2)/a(1);
-       b=X(1,2)-a*X(1,1);
 
-       p2=P2(i,2)- a*P2(i,1)-b;
-       p1=P1(:,2)- a*P1(:,1)-b;   
-     
-   end
-   
-   p2=p2/abs(p2);
-   p1=sum(p1./abs(p1));
-   p2*p1;
-   if p2*p1==-3
-       flag=false;
-       return
-   end
-end       
+        A2 = P2(mod(i+1, 3)+1, 2) - P2(mod(i, 3)+1, 2);
 
-% *******************************************************************
+        B2 = P2(mod(i, 3)+1, 1) - P2(mod(i+1, 3)+1, 1);
+
+        C2 = A2*P2(mod(i, 3)+1, 1)+B2*P2(mod(i, 3)+1, 2);
+
+
+
+        det = A1*B2 - A2*B1;
+
+
+
+        if det == 0
+
+            x = inf;
+
+            y = inf;
+
+        else
+
+            x = (B2*C1 - B1*C2)/det;
+
+            y = (A1*C2 - A2*C1)/det;
+
+        end
+
+
+
+        if (min(P1(mod(j, 3)+1, 1), P1(mod(j+1, 3)+1, 1)) <= x) && (x <= max(P1(mod(j, 3)+1, 1), P1(mod(j+1, 3)+1, 1))) && (min(P1(mod(j+1, 3)+1, 2), P1(mod(j, 3)+1, 2)) <= y) && (y <= max(P1(mod(j+1, 3)+1, 2), P1(mod(j, 3)+1, 2)))
+
+            if (min(P2(mod(i, 3)+1, 1), P2(mod(i+1, 3)+1, 1)) <= x) && (x <= max(P2(mod(i, 3)+1, 1), P2(mod(i+1, 3)+1, 1))) && (min(P2(mod(i+1, 3)+1, 2), P2(mod(i, 3)+1, 2)) <= y) && (y <= max(P2(mod(i+1, 3)+1, 2), P2(mod(i, 3)+1, 2)))
+
+            flag1 = true;
+
+            break;
+
+            end
+
+        else
+
+            flag1 = false;
+
+        end
+
+    end
+
+    if flag1 == true;
+
+        break;
+
+    end
+
+end
+
+
+
+%% Judge whether a point lies inside of a triangle
+
+
+
+for i = 1:3
+
+    b1 = sign(P2(i, :), P1(1, :), P1(2, :)) < 0.0;
+
+    b2 = sign(P2(i, :), P1(2, :), P1(3, :)) < 0.0;
+
+    b3 = sign(P2(i, :), P1(3, :), P1(1, :)) < 0.0;
+
+
+
+    flag2 =  ((b1 == b2) && (b2 == b3));
+
+    if flag2
+
+        break;
+
+    end
+
+end
+
+
+
+for i = 1:3
+
+    b1 = sign(P1(i, :), P2(1, :), P2(2, :)) < 0.0;
+
+    b2 = sign(P1(i, :), P2(2, :), P2(3, :)) < 0.0;
+
+    b3 = sign(P1(i, :), P2(3, :), P2(1, :)) < 0.0;
+
+
+
+    flag3 =  ((b1 == b2) && (b2 == b3));
+
+    if flag3
+
+        break;
+
+    end
+
+end
+
+
+
+if flag1 || flag2 || flag3
+
+    flag = true;
+
+else 
+
+    flag = false;
+
+end
+
+
+
+end
+
+
+
+function result = sign(p1, p2, p3)
+
+    result =  (p1(1) - p3(1)) * (p2(2) - p3(2)) - (p2(1) - p3(1)) * (p1(2) - p3(2));
+
 end
